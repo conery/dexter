bd74694f8eb606ed22d0def74b65680b42f1a7be
John Conery <jconery@icloud.com>
2025-10-27 12:28:58 -0700

    Add Log widget

--- src/dexter/gui.py
+++ src/dexter/gui.py
@@ -2,10 +2,12 @@
 # TUI (Terminal User Interface) implemented with Textual
 #
 
-from textual import log
+# from textual import log
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, VerticalScroll
-from textual.widgets import Footer, Header, Input, Tree
+from textual.widgets import Footer, Header, Log, Input, Tree
+
+from .util import debugging
 
 # Main app calls this method to launch the GUI
 
@@ -13,7 +15,7 @@ def start_gui(recs, args):
     '''
     Create the Textual app and start it
     '''
-    app = TUI()
+    app = TUI(recs, args)
     app.run()
 
 
@@ -21,6 +23,18 @@ class TUI(App):
     '''
     A Textual app for displaying and editing Dexter objects.
     '''
+
+    def __init__(self, recs, args) -> None:
+        '''
+        Constructor for the top level app.
+
+        Arguments:
+            recs:  list of objects to display in the table
+            args:  command line arguments
+        '''
+        self.records = recs
+        self.args = args
+        super().__init__()
 
     CSS_PATH = "dex.tcss"
 
@@ -35,9 +49,17 @@ class TUI(App):
         yield header
         yield Footer()
 
+        if debugging():
+            yield Log()
+
+    def add_message(self, message):
+        if debugging():
+            w = self.query_one(Log)
+            w.write_line(message)
+
     def on_mount(self):
         self.title = '\nDexter'
         self.sub_title = 'Double Entry Expense Tracker'
 
-
-
+    def on_ready(self):
+        self.add_message(f'display {len(self.records)} records')
